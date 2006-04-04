a07d13c895b350dceaea3e1b53c052b889b449d9
Tobias Klauser <tklauser@xenon.localdomain>
2006-04-04 13:41:21 +0200

    Add real header

--- simpletail.c
+++ simpletail.c
@@ -1,5 +1,24 @@
 /*
  * simpletail.c
+ * A fast implementation of tail which uses the inotify-API present in
+ * recent Linux Kernels.
+ *
+ * Copyright (C) 2005-2006, Tobias Klauser <tklauser@access.unizh.ch>
+ *
+ * The idea and some code were taken from turbotail.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #include <sys/types.h>
